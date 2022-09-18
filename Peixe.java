2c6ac96ccfd56795cc24debc27188c6324c98e60
Henrique Sicilia <siciliahenrique@gmail.com>
2022-09-18 16:03:21 -0300

    Create Peixe.java

---
+++ Peixe.java
@@ -0,0 +1,44 @@
+package polimorfismo1;
+
+public class Peixe extends Animal{
+	
+	private String corEscama;
+	
+	public void soltarBolha() {
+		
+		System.out.println("Soltou uma bolha");
+		
+	}
+
+	@Override
+	public void locomover() {
+		
+		System.out.println("Nadando");
+		
+	}
+
+	@Override
+	public void alimentar() {
+		
+		System.out.println("Comendo substâncias");
+		
+	}
+
+	@Override
+	public void emitirSom() {
+		
+		System.out.println("Som de Peixe");
+		
+	}
+	
+	public String getCorEscama() {
+		return corEscama;
+	}
+
+	public void setCorEscama(String corEscama) {
+		this.corEscama = corEscama;
+	}
+	
+	
+
+}
